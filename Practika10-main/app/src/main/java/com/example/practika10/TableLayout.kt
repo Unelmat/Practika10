beef27923acbe7f1212f0391e9ff6da48ff77b94
Sasik-popishak <151189640+Sasik-popishak@users.noreply.github.com>
2023-11-23 16:34:36 +0300

    Practica10
    Practica10

---
+++ Practika10-main/app/src/main/java/com/example/practika10/TableLayout.kt
@@ -0,0 +1,19 @@
+package com.example.practika10
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class TableLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+    fun btnChangeActivity(view: View){
+        val btn = view as Button
+        val intent: Intent = Intent(this@TableLayout, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
